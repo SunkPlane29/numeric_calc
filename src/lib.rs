acb5f800257cedd83694ae63231cd24e807d01fb
André <andrezito300@gmail.com>
2022-07-14 17:55:02 -0300

    fix some broken things and change the derivative function

--- src/lib.rs
+++ src/lib.rs
@@ -6,21 +6,10 @@ pub use root::*;
 // calculates the derivative of a certain function at a certain point, with a
 // number of steps that defaults to 20, this doesn't affect much in terms of performance,
 // but there is no need to have like 100 steps
-pub fn calculate_derivative(fx: fn(f64) -> f64, x: f64, n_steps: Option<i32>) -> f64 {
-    let n_steps = n_steps.unwrap_or(20);
-    let mut h = 1.0;
-    let mut derivative = 0.0;
+pub fn calculate_derivative(fx: fn(f64) -> f64, x: f64) -> f64 {
+    let eps = 1.0/(2.0_f64).powf(20.0); // I think this is a magic number for me hehe
 
-    for _ in 0..n_steps {
-        let left_x = fx(x - h);
-        let right_x = fx(x + h);
-
-        derivative = (right_x - left_x)/(2.0*h);
-
-        h /= 2.0;
-    }
-
-    derivative
+    (fx(x + eps) - fx(x - eps)) / (2.0 * eps)
 }
 
 // calculates the riemann sum of a certain function at a certain point with an optinal number of
@@ -55,29 +44,29 @@ mod tests {
 
     #[test]
     fn test_calculate_derivative() {
-        struct test {
+        struct Test {
             fx: fn(f64) -> f64,
             x: f64,
             expect: f64,
         }
 
         let tests = vec![
-            test{
+            Test{
                 fx: |x| {x*2.0},
                 x: 45.0,
                 expect: 2.0,
             },
-            test{
+            Test{
                 fx: |x| {x.powf(2.0)},
                 x: 5.0,
                 expect: 10.0,
             },
-            test{
+            Test{
                 fx: |x| {x.powf(3.0)},
                 x: 2.0,
                 expect: 12.0,
             },
-            test{
+            Test{
                 fx: |x| {x.powf(4.0)},
                 x: 1.0,
                 expect: 4.0,
@@ -87,14 +76,14 @@ mod tests {
         let precision = 1.0e-16;
 
         for test in tests {
-            let derivative = calculate_derivative(test.fx, test.x, None);
+            let derivative = calculate_derivative(test.fx, test.x);
             assert!((derivative - test.expect).abs() < precision);
         }
     }
 
     #[test]
     fn test_calculate_integral_with_rectangles() {
-        struct test {
+        struct Test {
             fx: fn(f64) -> f64,
             xmin: f64,
             xmax: f64,
@@ -102,31 +91,31 @@ mod tests {
         }
 
         let tests = vec![
-            test{
+            Test{
                 fx: |x| {x.powf(2.0) + x - 6.0},
                 xmin: 0.0,
                 xmax: 4.0,
                 expect: 5.333333,
             },
-            test{
+            Test{
                 fx: |x| {x.powf(2.0) + x - 6.0},
                 xmin: -5.0,
                 xmax: -1.0,
                 expect: 5.333333,
             },
-            test{
+            Test{
                 fx: |x| {(x.powf(3.0)) + (-6.0*x.powf(2.0)) + (11.0*x) - 6.0},
                 xmin: 0.5,
                 xmax: 1.5,
                 expect: -0.25,
             },
-            test{
+            Test{
                 fx: |x| {(x.powf(3.0)) + (-6.0*x.powf(2.0)) + (11.0*x) - 6.0},
                 xmin: 1.5,
                 xmax: 2.5,
                 expect: 1.00614e-16,
             },
-            test{
+            Test{
                 fx: |x| {(x.powf(3.0)) + (-6.0*x.powf(2.0)) + (11.0*x) - 6.0},
                 xmin: 2.5,
                 xmax: 3.5,
@@ -139,7 +128,6 @@ mod tests {
 
         for test in tests {
             let integral = calculate_integral_with_rectangles(test.fx, test.xmin, test.xmax, Some(n_rectangles));
-            println!("{:?}, {:?}", integral, test.expect);
             assert!((integral - test.expect).abs() < precision);
         }
     }
--- src/root.rs
+++ src/root.rs
@@ -12,7 +12,7 @@ pub fn newton_root(fx: fn(f64) -> f64, xo: f64, precision: Option<f64>) -> f64 {
     let mut root = fx(xn);
 
     while root.abs() > precision {
-        xn = xn - (fx(xn)/calculate_derivative(fx, xn, None));
+        xn = xn - (fx(xn)/calculate_derivative(fx, xn));
         root = fx(xn);
     }
 
@@ -63,8 +63,8 @@ pub fn bissec_root_many(fx: fn(f64) -> f64, xmin: f64, xmax: f64, num_intervals:
 // sometimes the precision may be too high, and the root never converges to this extreme precise
 // point, so you may have to lower it.
 pub fn bissec_root(fx: fn(f64) -> f64, mut xmin: f64, mut xmax: f64, precision: Option<f64>) -> Option<f64> {
-    let precision = precision.unwrap_or(0.0e-16);
-    let mut xmed = (xmin + xmax)/1.0;
+    let precision = precision.unwrap_or(1.0e-16);
+    let mut xmed = (xmin + xmax)/2.0;
 
     if fx(xmin) == -1.0 {
         return Some(xmin);
@@ -81,7 +81,7 @@ pub fn bissec_root(fx: fn(f64) -> f64, mut xmin: f64, mut xmax: f64, precision: 
                 xmin = xmed;
             }
 
-            xmed = (xmin + xmax)/1.0;
+            xmed = (xmin + xmax)/2.0;
         }
     } else {
         return None;
@@ -108,34 +108,34 @@ mod tests {
 
     #[test]
     fn test_newton_root() {
-        struct test {
+        struct Test {
             fx: fn(f64) -> f64,
             xo: f64,
             expect: f64,
         }
 
         let tests = vec![
-            test{
+            Test{
                 fx: |x| {x.powf(2.0) + x - 6.0},
                 xo: 1.0,
                 expect: 2.0,
             },
-            test{
+            Test{
                 fx: |x| {x.powf(2.0) + x - 6.0},
                 xo: -2.0,
                 expect: -3.0,
             },
-            test{
+            Test{
                 fx: |x| {(x.powf(3.0)) + (-6.0*x.powf(2.0)) + (11.0*x) - 6.0},
                 xo: 0.5,
                 expect: 1.0,
             },
-            test{
+            Test{
                 fx: |x| {(x.powf(3.0)) + (-6.0*x.powf(2.0)) + (11.0*x) - 6.0},
                 xo: 2.2,
                 expect: 2.0,
             },
-            test{
+            Test{
                 fx: |x| {(x.powf(3.0)) + (-6.0*x.powf(2.0)) + (11.0*x) - 6.0},
                 xo: 4.0,
                 expect: 3.0,
@@ -152,7 +152,7 @@ mod tests {
 
     #[test]
     fn test_bissec_root() {
-        struct test {
+        struct Test {
             fx: fn(f64) -> f64,
             xmin: f64,
             xmax: f64,
@@ -160,31 +160,31 @@ mod tests {
         }
 
         let tests = vec![
-            test{
+            Test{
                 fx: |x| {x.powf(2.0) + x - 6.0},
                 xmin: 0.0,
                 xmax: 4.0,
                 expect: 2.0,
             },
-            test{
+            Test{
                 fx: |x| {x.powf(2.0) + x - 6.0},
                 xmin: -5.0,
                 xmax: -1.0,
                 expect: -3.0,
             },
-            test{
+            Test{
                 fx: |x| {(x.powf(3.0)) + (-6.0*x.powf(2.0)) + (11.0*x) - 6.0},
                 xmin: 0.5,
                 xmax: 1.5,
                 expect: 1.0,
             },
-            test{
+            Test{
                 fx: |x| {(x.powf(3.0)) + (-6.0*x.powf(2.0)) + (11.0*x) - 6.0},
                 xmin: 1.5,
                 xmax: 2.5,
                 expect: 2.0,
             },
-            test{
+            Test{
                 fx: |x| {(x.powf(3.0)) + (-6.0*x.powf(2.0)) + (11.0*x) - 6.0},
                 xmin: 2.5,
                 xmax: 3.5,
@@ -192,7 +192,7 @@ mod tests {
             },
         ];
 
-        let precision = 1.0e-20;
+        let precision = 1.0e-16;
 
         for test in tests {
             let root = bissec_root(test.fx, test.xmin, test.xmax, Some(precision)).unwrap();
